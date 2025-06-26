dd29607e9505090c6dfd8babc8645af2de0c41f8
rckr2710 <rckr2710@gmail.com>
2025-06-26 12:21:17 +0530

    Updated

--- main.py
+++ main.py
@@ -319,16 +319,28 @@ def close_gitlab_issue(issue_id: int, get_current_user: str = Depends(get_curren
         raise HTTPException(status_code=500, detail=str(e))
     
 @app.put("/gitlab-issue/edit-issue/{issue_id}")
-def edit_gitlab_issue(issue_id: int, title: str = Form(...), description: str = Form(...),image: Optional[UploadFile] = File(None) , get_current_user: str = Depends(get_current_user)):
+async def edit_gitlab_issue(issue_id: int, title: str = Form(...), description: str = Form(...),image: Optional[UploadFile] = File(None) , get_current_user: str = Depends(get_current_user)):
     if not issue_id:
         raise HTTPException(status_code=400, detail="Issue ID is required")
     url = f"{GITLAB_URL}/api/v4/projects/{GITLAB_PROJECT_ID}/issues/{issue_id}"
     headers = {
         "PRIVATE-TOKEN": GITLAB_PRIVATE_TOKEN,
     }
+    image_markdown = ""
+    if image:
+        contents = await image.read()
+        files = {"file": (image.filename, contents, image.content_type)}
+        async with httpx.AsyncClient(timeout=10) as client:
+            upload_url = f"{GITLAB_URL}/api/v4/projects/{GITLAB_PROJECT_ID}/uploads"
+            upload_resp = await client.post(upload_url, headers=headers, files=files)
+            if upload_resp.status_code != 201:
+                raise HTTPException(status_code=upload_resp.status_code, detail=upload_resp.text)
+            upload_data = upload_resp.json()
+            image_url = upload_data["url"]
+            image_markdown = f"\n\n![{upload_data['alt']}]({GITLAB_URL}{image_url})"
     data = {
         "title": title,
-        "description": description,
+        "description": f"{description}{image_markdown}",
         "type" : "ISSUE"
     }
     try:
