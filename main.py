7500a0f8d39137852ab0e679409b7b92113a0136
rckr2710 <rckr2710@gmail.com>
2025-07-02 13:24:36 +0530

    update

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
